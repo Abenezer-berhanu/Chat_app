057e8df10f72b329090eefa25efc0c6316510655
Abenezer <abenezerberhanu40@gmail.com>
2024-02-07 08:48:16 +0300

    add emoji to input

--- src/components/UiComponents/MessageInput.tsx
+++ src/components/UiComponents/MessageInput.tsx
@@ -1,24 +1,47 @@
 "use client";
-import { SendHorizontal } from "lucide-react";
+import { SendHorizontal, SmilePlus } from "lucide-react";
 import { useState } from "react";
+import EmojiPicker from "emoji-picker-react";
 
 function MessageInput() {
   const [input, setInput] = useState("");
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value);
+  const [emoji, setEmoji] = useState(false);
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    emoji?: string
+  ) => {
+    if (emoji) {
+      //@ts-ignore
+      setInput((prev) => prev + e.emoji);
+    } else {
+      setEmoji(false);
+      setInput(e.target.value);
+    }
   };
   return (
     <div className="h-14 relative">
       <input
         type="text"
-        className="indent-5 relative rounded-full w-full h-full border border-primary outline-none"
+        className="indent-5 rounded-full w-full h-full border border-primary outline-none"
         value={input}
         onChange={handleChange}
         placeholder="Type a Message"
       />
-      {input && (
-        <SendHorizontal className="absolute text-primary right-4 top-0 bottom-0 my-auto cursor-pointer" />
-      )}
+      <div className="flex">
+        {input && (
+          <SendHorizontal className="absolute text-primary right-4 top-0 bottom-0 my-auto cursor-pointer" />
+        )}
+        <div className="absolute top-[-500px] left-0">
+          <EmojiPicker
+            open={emoji}
+            onEmojiClick={(e: any) => handleChange(e, "emoji")}
+          />
+        </div>
+        <SmilePlus
+          className="absolute text-primary right-4 top-0 bottom-0 my-auto cursor-pointer"
+          onClick={() => setEmoji(!emoji)}
+        />
+      </div>
     </div>
   );
 }

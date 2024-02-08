e07c5fb0a6ba578a74167a7f8d15e6d679ec1614
Abenezer <abenezerberhanu40@gmail.com>
2024-02-08 11:53:25 +0300

    add user google Oauth login functionality

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,23 @@
-import { auth, signIn } from "@/lib/auth";
+import { signIn } from "@/lib/auth";
 
 async function login() {
-  const session = await auth();
   const handleGitHubLogin = async () => {
     "use server";
     await signIn("github");
   };
-
-  console.log(session);
+  const handleGoogleLogin = async () => {
+    "use server";
+    await signIn("google");
+  };
   return (
-    <form action={handleGitHubLogin}>
-      <button>Login</button>
-    </form>
+    <>
+      <form action={handleGitHubLogin}>
+        <button>Login</button>
+      </form>
+      <form action={handleGoogleLogin}>
+        <button>Login google</button>
+      </form>
+    </>
   );
 }
 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 export const {
   handlers: { GET, POST },
   auth,
@@ -10,6 +11,10 @@ export const {
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID,
       clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Google({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 });
